117cbf7797d0c573e68df25be5a1beaae315b7d7
Joshua Carroll <carrolljosh1976@gmail.com>
2022-08-17 22:00:17 -0400

    started personal project

---
+++ backend/functions/src/index.ts
@@ -0,0 +1,13 @@
+import * as functions from 'firebase-functions';
+import express from 'express';
+import cors from 'cors';
+
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/',)
+export const api = functions.https.onRequest(app);
